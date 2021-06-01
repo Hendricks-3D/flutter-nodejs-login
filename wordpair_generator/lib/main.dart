4eae3a57c666232aaedd43b0f47a7116fb96186d
obrown <odain.brown@alvys.com>
2021-06-01 12:08:21 -0500

    favorite icon added

--- wordpair_generator/lib/main.dart
+++ wordpair_generator/lib/main.dart
@@ -2,6 +2,10 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
+import 'random_word.dart';
+
+
 void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget{
@@ -13,54 +17,5 @@ class MyApp extends StatelessWidget{
        theme:ThemeData(primaryColor: Colors.purple[900]),
        home: RandomWords());
         
-  }
-}
-
-
-class RandomWords extends StatefulWidget{
-  @override 
-  RandomWordsState createState() => RandomWordsState();
-  }
-  
-  class RandomWordsState extends State<RandomWords> {
-
-    final string_randomWordPairs = <WordPair>[];
-    Widget _buildList(){
-
-      return ListView.builder(
-              padding: const EdgeInsets.all(16.0),
-              itemBuilder: (context, item){
-                if(item.isOdd)
-                   return Divider();
-                final int index = item ~/2;//Calculate the number of word pai that is in the listView minus the divider widget
-
-                //generate new pairs on scroll
-                if(index>=string_randomWordPairs.length){
-                  string_randomWordPairs.addAll(generateWordPairs().take(10));
-                  
-                }
-
-                return _buildRow(string_randomWordPairs[index]);
-              },
-        );
-    }
-
-    //Widget that builds the list
-    Widget build(BuildContext context){
-      return Scaffold(  
-         appBar:AppBar(
-           title: Text('WordPair Generator')),
-         body:_buildList(),
-       );
-    }
-
-  //Row widgets that accepts a random word and add to the row
-  Widget _buildRow(WordPair word) {
-
-      return ListTile(
-        title:Text(word.asPascalCase,
-              style: 
-              TextStyle(fontSize: 18.0),)
-      );
   }
 }
---
+++ wordpair_generator/lib/random_word.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+
+
+class RandomWords extends StatefulWidget{
+  @override 
+  RandomWordsState createState() => RandomWordsState();
+  }
+  
+  class RandomWordsState extends State<RandomWords> {
+
+    final _randomWordPairs = <WordPair>[];
+    final _savedWordPairs = Set<WordPair>();//A set is a collection of object whereneach object is unique.
+
+    Widget _buildList(){
+
+      return ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemBuilder: (context, item){
+                if(item.isOdd)
+                   return Divider();
+                final int index = item ~/2;//Calculate the number of word pai that is in the listView minus the divider widget
+
+                //generate new pairs on scroll
+                if(index >=_randomWordPairs.length){
+                  _randomWordPairs.addAll(generateWordPairs().take(10));
+                  
+                }
+
+                return _buildRow(_randomWordPairs[index]);
+              },
+        );
+    }//End _buildList Widget
+
+
+
+//----Widget that builds the list
+    Widget build(BuildContext context){
+      return Scaffold(  
+         appBar:AppBar(
+           title: Text('WordPair Generator')),
+         body:_buildList(),
+       );
+    }
+
+
+  //-----Row widgets that accepts a random word and add to the row
+  Widget _buildRow(WordPair word) {
+      final alreadySaved = _savedWordPairs.contains(word);
+
+      return ListTile(
+        title:Text(word.asPascalCase,
+              style: 
+              TextStyle(fontSize: 18.0),),
+        trailing: Icon(alreadySaved? Icons.favorite:Icons.favorite_border,//add icon if pair is already saved
+              color: alreadySaved ? Colors.red: null),//add color if pair is already saved using ternary operation on the color widget
+      );
+  }
+}
