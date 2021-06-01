4d0c6205fc4c0816746cee9fce3dab28c8e09feb
obrown <odain.brown@alvys.com>
2021-06-01 11:50:21 -0500

    infinite word list created

--- wordpair_generator/lib/main.dart
+++ wordpair_generator/lib/main.dart
@@ -7,14 +7,60 @@ void main()=>runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override 
   Widget build(BuildContext context){
-     final wordPair = WordPair.random();//creates a single word pair randomly
+     //final wordPair = WordPair.random();//creates a single word pair randomly
 
      return MaterialApp(
        theme:ThemeData(primaryColor: Colors.purple[900]),
-       home: Scaffold(  
-         appBar:AppBar(title: Text('WordPair Generator')),
-         body:Center( child: Text(wordPair.asPascalCase),)
-       ) , );
+       home: RandomWords());
         
+  }
+}
+
+
+class RandomWords extends StatefulWidget{
+  @override 
+  RandomWordsState createState() => RandomWordsState();
+  }
+  
+  class RandomWordsState extends State<RandomWords> {
+
+    final string_randomWordPairs = <WordPair>[];
+    Widget _buildList(){
+
+      return ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemBuilder: (context, item){
+                if(item.isOdd)
+                   return Divider();
+                final int index = item ~/2;//Calculate the number of word pai that is in the listView minus the divider widget
+
+                //generate new pairs on scroll
+                if(index>=string_randomWordPairs.length){
+                  string_randomWordPairs.addAll(generateWordPairs().take(10));
+                  
+                }
+
+                return _buildRow(string_randomWordPairs[index]);
+              },
+        );
+    }
+
+    //Widget that builds the list
+    Widget build(BuildContext context){
+      return Scaffold(  
+         appBar:AppBar(
+           title: Text('WordPair Generator')),
+         body:_buildList(),
+       );
+    }
+
+  //Row widgets that accepts a random word and add to the row
+  Widget _buildRow(WordPair word) {
+
+      return ListTile(
+        title:Text(word.asPascalCase,
+              style: 
+              TextStyle(fontSize: 18.0),)
+      );
   }
 }
