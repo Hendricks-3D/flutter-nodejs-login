e7bc8ca980b13b91039a3543e37a3bae6254a238
obrown <odain.brown@alvys.com>
2021-06-01 11:11:39 -0500

    Initial commit

---
+++ wordpair_generator/lib/main.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+     final wordPair = WordPair.random();//creates a single word pair randomly
+
+     return MaterialApp(
+       theme:ThemeData(primaryColor: Colors.purple[900]),
+       home: Scaffold(  
+         appBar:AppBar(title: Text('WordPair Generator')),
+         body:Center( child: Text(wordPair.asPascalCase),)
+       ) , );
+        
+  }
+}
