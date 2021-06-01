a40434265b64e5323b95fa7bef2d69b59b7ed1bc
obrown <odain.brown@alvys.com>
2021-06-01 15:06:23 -0500

    Saved words page added

--- wordpair_generator/lib/main.dart
+++ wordpair_generator/lib/main.dart
@@ -1,21 +1,18 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 import 'random_word.dart';
 
+void main() => runApp(MyApp());
 
-void main()=>runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random();//creates a single word pair randomly
 
-class MyApp extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-     //final wordPair = WordPair.random();//creates a single word pair randomly
-
-     return MaterialApp(
-       theme:ThemeData(primaryColor: Colors.purple[900]),
-       home: RandomWords());
-        
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: RandomWords());
   }
 }
--- wordpair_generator/lib/random_word.dart
+++ wordpair_generator/lib/random_word.dart
@@ -1,60 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-
-
-class RandomWords extends StatefulWidget{
-  @override 
+class RandomWords extends StatefulWidget {
+  @override
   RandomWordsState createState() => RandomWordsState();
-  }
-  
-  class RandomWordsState extends State<RandomWords> {
+}
 
-    final _randomWordPairs = <WordPair>[];
-    final _savedWordPairs = Set<WordPair>();//A set is a collection of object whereneach object is unique.
+class RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<
+      WordPair>(); //A set is a collection of object whereneach object is unique.
 
-    Widget _buildList(){
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return Divider();
+        final int index = item ~/
+            2; //Calculate the number of word pai that is in the listView minus the divider widget
 
-      return ListView.builder(
-              padding: const EdgeInsets.all(16.0),
-              itemBuilder: (context, item){
-                if(item.isOdd)
-                   return Divider();
-                final int index = item ~/2;//Calculate the number of word pai that is in the listView minus the divider widget
+        //generate new pairs on scroll
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
 
-                //generate new pairs on scroll
-                if(index >=_randomWordPairs.length){
-                  _randomWordPairs.addAll(generateWordPairs().take(10));
-                  
-                }
-
-                return _buildRow(_randomWordPairs[index]);
-              },
-        );
-    }//End _buildList Widget
-
-
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  } //End _buildList Widget
 
 //----Widget that builds the list
-    Widget build(BuildContext context){
-      return Scaffold(  
-         appBar:AppBar(
-           title: Text('WordPair Generator')),
-         body:_buildList(),
-       );
-    }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('WordPair Generator'), actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.list),
+          onPressed: _PushSaved(),
+        )
+      ]),
+      body: _buildList(),
+    );
+  }
 
+  _PushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair word) {
+        return ListTile(
+            title: Text(word.asPascalCase, style: TextStyle(fontSize: 16.0)));
+      });
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Saved Words'),
+          ),
+          body: ListView(children: divided));
+    }));
+  }
 
   //-----Row widgets that accepts a random word and add to the row
   Widget _buildRow(WordPair word) {
-      final alreadySaved = _savedWordPairs.contains(word);
+    final alreadySaved = _savedWordPairs.contains(word);
 
-      return ListTile(
-        title:Text(word.asPascalCase,
-              style: 
-              TextStyle(fontSize: 18.0),),
-        trailing: Icon(alreadySaved? Icons.favorite:Icons.favorite_border,//add icon if pair is already saved
-              color: alreadySaved ? Colors.red: null),//add color if pair is already saved using ternary operation on the color widget
-      );
+    return ListTile(
+      title: Text(
+        word.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
+      ),
+      trailing: Icon(
+          alreadySaved
+              ? Icons.favorite
+              : Icons.favorite_border, //add icon if pair is already saved
+          color: alreadySaved
+              ? Colors.red
+              : null), //add color if pair is already saved using ternary operation on the color widget
+
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _savedWordPairs.remove(word);
+          } else {
+            _savedWordPairs.add(word);
+          }
+        });
+      },
+    );
   }
 }
